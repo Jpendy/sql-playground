cbbf79c75b8c038ea882d9bbbc1689ae87a22397
Jpendy <Jpendy256@gmail.com>
2020-11-08 23:02:14 -0800

    all crud routes made

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,44 +1,137 @@
 /* eslint-disable space-before-function-paren */
-const { expect, afterEach } = require('@jest/globals');
+const { expect, afterAll, it, beforeAll } = require('@jest/globals');
+const { any } = require('expect');
 const fs = require('fs');
+const { parse } = require('path');
 const request = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
-// const Team = require('../lib/models/team');
+const Team = require('../lib/models/team');
 
 describe('tests routes', () => {
-    beforeEach(async () => {
+
+    beforeAll(async () => {
         await client.connect();
-        return await client.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
     });
 
-    afterEach(() => {
-        client.end();
+    beforeEach(async () => {
+        await client.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+
+        await Promise.all([
+            {
+                name: 'Portland Trail Blazers',
+                location: 'Portland, Oregon'
+            },
+            {
+                name: 'Denver Nuggets',
+                location: 'Denver, Colorado'
+            }
+        ].map(item => Team.create(item)));
+    });
+
+    afterAll(async () => {
+        await client.end();
     });
 
     // eslint-disable-next-line space-before-function-paren
     it('it creates a new team with INSERT', async () => {
 
-        // const team = await Team.create({
-        //     name: 'Portland Trail Blazers',
-        //     location: 'Portland'
-        // });
-
         const createdTeam = await request(app)
             .post('/api/v1/teams')
             .send({
-                id: 1,
-                name: 'Portland Trail Blazers',
-                location: 'Portland'
+                name: 'Dallas Mavericks',
+                location: 'Dallas Texas'
             });
 
+        const parsedResponse = JSON.parse(createdTeam.text);
 
-        expect(createdTeam).toEqual({
-            id: 1,
-            name: 'Portland Trail Blazers',
-            location: 'Portland'
+        expect(parsedResponse).toEqual({
+            id: '3',
+            name: 'Dallas Mavericks',
+            location: 'Dallas Texas'
         });
     });
 
-    expect();
+    it('it gets all the teams with SELECT *', async () => {
+
+        const teams = await request(app)
+            .get('/api/v1/teams');
+
+        const parsedResponse = JSON.parse(teams.text);
+
+        expect(parsedResponse).toEqual([
+            {
+                id: expect.anything(),
+                name: 'Portland Trail Blazers',
+                location: 'Portland, Oregon'
+            },
+            {
+                id: expect.anything(),
+                name: 'Denver Nuggets',
+                location: 'Denver, Colorado'
+            }
+        ]);
+    });
+
+    it('it gets a single team by id', async () => {
+
+        const team = await Team.create({
+            name: 'Toronto Raptors',
+            location: 'Toronto, Canada'
+        });
+
+        const teamRes = await request(app)
+            .get(`/api/v1/teams/${team.id}`);
+
+        const parsedResponse = JSON.parse(teamRes.text);
+
+        expect(parsedResponse).toEqual({
+            id: team.id,
+            name: 'Toronto Raptors',
+            location: 'Toronto, Canada'
+        });
+    });
+
+    it('it can update a team by id', async () => {
+
+        const team = await Team.create({
+            name: 'Toronto Raptors',
+            location: 'Toronto, Canada'
+        });
+
+        const updatedTeam = await request(app)
+            .put(`/api/v1/teams/${team.id}`)
+            .send({
+                name: 'Toronto Raptors',
+                location: 'Toronto, Maple Syrup Land'
+            });
+
+        const parsedResponse = JSON.parse(updatedTeam.text);
+
+        expect(parsedResponse).toEqual({
+            id: team.id,
+            name: 'Toronto Raptors',
+            location: 'Toronto, Maple Syrup Land'
+        });
+
+    });
+
+    it('it deletes a team by id', async () => {
+        const team = await Team.create({
+            name: 'Toronto Raptors',
+            location: 'Toronto, Canada'
+        });
+
+        const deletedTeam = await request(app)
+            .delete(`/api/v1/teams/${team.id}`);
+
+        // const parsedResponse = JSON.parse(deletedTeam.text);
+        const parsedResponse = deletedTeam;
+
+        expect(parsedResponse.text).toEqual({
+            id: team.id,
+            name: 'Toronto Raptors',
+            location: 'Toronto, Canada'
+        });
+    });
 });
